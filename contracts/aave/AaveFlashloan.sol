e8b8684ef6d46204cc791a044bd199946ad9f0af
mmazambo <mmazambo@yahoo.fr>
2024-06-03 21:07:48 +1000

    Update AaveFlashloan.sol

--- contracts/aave/AaveFlashloan.sol
+++ contracts/aave/AaveFlashloan.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.10;
 
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import FlashLoan"@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 import "./IPool.sol";
